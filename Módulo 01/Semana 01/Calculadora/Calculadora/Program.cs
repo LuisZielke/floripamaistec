992f8e250782eb30140da66a424dd16db688dcf9
LuisZielke <luiszielke89@gmail.com>
2023-02-07 21:30:55 -0300

    Meu primeiro commit

---
+++ Módulo 01/Semana 01/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿//Importação de bibliotecas
+
+//Nome da classe
+
+//Declaração de método
+
+//Declaração de variáveis
+int opcao, numero1 = 0, numero2 = 0;
+float resultado = 0;
+
+
+//Entradas
+Console.WriteLine("Escolha uma operação: \n\n" +
+    "1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão \n\n" +
+    "5 - Sair\n\nDigite a opção escolhida: ");
+
+opcao = int.Parse(Console.ReadLine());
+
+    //Processamento
+    switch(opcao)
+{
+    //Bloco de instruções da estrutura condicional
+    case 1:
+        Console.WriteLine("operação somar.");
+        Console.Write("Digite o numero 1: ");
+        numero1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o numero 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+        resultado = numero1 + numero2;
+        break;
+    case 2:
+        Console.WriteLine("operação subtrair.");
+        break;
+    case 3:
+        Console.WriteLine("operação multiplicar.");
+        break;
+    case 4:
+        Console.WriteLine("operação dividir.");
+        break;
+    case 5:
+        Console.WriteLine("operação sair.");
+        break;
+
+}
+Console.WriteLine("Resultado: " + resultado);
+
+    //Saídas
+
+
---
+++ Módulo 01/Semana 01/olaMundo/olaMundo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numeroInteiro = 10; // Declaração de variável inteira, recebe o valor 10
+long numeroMaior = 1099999L;//Declaração de variável long, recebendo valor 1099999
+float numeroQuebrado = 6.8f;//Declaração variável de tipo float,
+double numeroQuebradomaior = 101.9999999D;// declaração de variavel do tipo double
+char letra = 'z';// Char recebendo um caracter
+string texto = "A turma estreito é a melhor."; // Recebendo texto
+bool variavelBooleana = true; //Booleano, true or false
+DateTime data = DateTime.Now;//datetime declara até os segundos do momento da aplicação
+DateTime outraData = new DateTime(2023, 01, 31);//Instanciando 
+texto.ToUpper();//Convertendo o texto para letras maiusculas
+Console.WriteLine("Frase com letras maiusculas: "+texto.ToUpper());
+
+string nomeCompleto; //Declaração de variável
+Console.WriteLine ("Digite o seu nome: ");//Instrução ao usuário
+nomeCompleto = Console.ReadLine();//aguardando o usuario digitar no terminal
+Console.WriteLine("O seu nome é: " + nomeCompleto);//Saída de dados concatenando texto com variável
+
+
