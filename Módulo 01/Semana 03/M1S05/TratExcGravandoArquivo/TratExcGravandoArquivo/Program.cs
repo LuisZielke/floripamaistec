c27362e53bf8452975af8669ec2d025c84ffbb0d
LuisZielke <luiszielke89@gmail.com>
2023-02-28 20:16:23 -0300

    exercio 2 semana 5

---
+++ Módulo 01/Semana 03/M1S05/TratExcGravandoArquivo/TratExcGravandoArquivo/Program.cs
@@ -0,0 +1,33 @@
+﻿
+	
+        FileInfo arquivo = null;
+
+		try
+		{
+
+
+			Console.WriteLine("digite o nome do arquivo que você vai criar:");
+			string nomeArquivo = Console.ReadLine();
+
+			arquivo = new FileInfo(nomeArquivo);
+
+			using (StreamWriter gravadorFluxo = File.AppendText(nomeArquivo))
+			{
+				gravadorFluxo.WriteLine("Texte de dentro do arquivo");
+			}
+		}
+		catch(ArgumentException ex)
+		{
+		Console.WriteLine($"Tratando o erro especificamente:{ex}");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Ocorreu um erro:{ex}");
+		}
+		finally
+		{
+			Console.WriteLine($"Encerrando programa..\n");
+			/*arquivo = null;
+			Console.Clear();*/
+		}
+	
