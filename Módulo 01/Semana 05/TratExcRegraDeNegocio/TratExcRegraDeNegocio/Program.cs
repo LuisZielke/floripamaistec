b4799d7c14657acc3c4e68ae5c27b358abaa6032
LuisZielke <luiszielke89@gmail.com>
2023-02-28 21:43:48 -0300

    Exercicio 3 semana 5

---
+++ Módulo 01/Semana 05/TratExcRegraDeNegocio/TratExcRegraDeNegocio/Program.cs
@@ -0,0 +1,16 @@
+﻿using TratExcRegraDeNegocio;
+
+Evento evento = new Evento();
+
+evento.QtdIngressosVendidos = 15;
+evento.QtdLugares = 20;
+
+if (evento.QtdLugares - evento.QtdIngressosVendidos > 0)
+{
+    Console.WriteLine("Ingresso validado");
+    evento.ingressoVendido();
+}
+else
+{
+    throw new IngressoException("Não existem mais lugares disponíveis para o evento!");
+}
